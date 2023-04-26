1bd9ed616b892214435713005f9b8a7506f4fb14
Benjamin Darbonne <ben@elevator3.com>
2023-04-26 09:17:01 -0500

    working script

--- dev-cert.sh
+++ dev-cert.sh
@@ -4,9 +4,14 @@ org=localhost-ca
 domain=localhost
 path="$HOME/.aspnet/https"
 
+if [ ! -d $path ]
+then
+	mkdir -p $path
+fi
+
 openssl genpkey -algorithm RSA -out "$path/ca.key"
 openssl req -x509 -key "$path/ca.key" -out "$path/ca.crt" \
-    -subj "/CN=$org/O=$org"
+   -subj "/CN=$org/O=$org"
 
 openssl genpkey -algorithm RSA -out "$path/$domain".key
 openssl req -new -key "$path/$domain".key -out "$path/$domain".csr \
@@ -24,5 +29,8 @@ END
 
 openssl pkcs12 -export -out "$path/$domain".pfx -inkey "$path/$domain".key -in "$path/$domain".crt
 
-#Arch Linux trust certificate system wide
-#sudo trust anchor $HOME/.aspnet/https/ca.crt
+
+
+certutil -d "sql:$HOME/.pki/nssdb" -A -i $path/ca.crt -n 'Localhost CA' -t C,,
+sudo trust anchor $HOME/.aspnet/https/ca.crt
+sudo update-ca-trust
